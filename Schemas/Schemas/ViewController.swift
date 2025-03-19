4a3f1a83c405c16cee148634fbd986f3dda62bd0
Luis Diego Ruiz Bautista <luis290613@gmail.com>
2025-03-19 11:23:48 -0500

    first commit

---
+++ Schemas/Schemas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schemas
+//
+//  Created by Luis Diego Ruiz Bautista on 19/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
